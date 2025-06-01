b3e82f6c3eabbe382a48bcdefc95b7a8e38233fd
kaviyarasan Murugan <kaviyarasanvv@gmail.com>
2025-06-01 15:10:15 +0530

    Update BookServiceApplication.java

--- src/main/java/com/yourcompany/bookservice/BookServiceApplication.java
+++ src/main/java/com/yourcompany/bookservice/BookServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableEurekaServer
 public class BookServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
